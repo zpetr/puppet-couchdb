c079c9d41a67ad5a0503da6042db5fc64f4a86d1
zpetr <petr.glu@hotmail.com>
2018-06-29 13:06:09 +0200

    Spec fixes

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 describe 'couchdb' do
-  context 'with defaults for all parameters' do
-    it { is_expected.to contain_class('couchdb') }
+  it { is_expected.to compile }
+  context 'with defaults' do
+    it { is_expected.to contain_class('couchdb::instance') }
   end
 end
