d235da3dd2df332fcf8d30a06cc20a1a9dfd0a8d
zpetr <petr.glu@hotmail.com>
2018-06-29 13:34:45 +0200

    Spec fixes

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
+
 describe 'couchdb' do
-  it { is_expected.to compile }
-  context 'with defaults' do
-    it { is_expected.to contain_class('couchdb::instance') }
+  let(:facts) do
+    {
+      :osfamily => 'Debian' ,
+      :operatingsystem => 'Linux' 
+    }
   end
+  it { is_expected.to compile }
 end
