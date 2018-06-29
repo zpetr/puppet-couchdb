1f4f433a2e14bcd77d56759ac4570680076d9f37
zpetr <petr.glu@hotmail.com>
2018-06-29 17:32:18 +0200

    Spec fixes

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,9 +4,10 @@ describe 'couchdb' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge(
+          root_home: '/root',
+        )
       end
-      let(:root_home) { '/root' }
 
       it { is_expected.to compile }
       it { is_expected.to contain_couchdb }
